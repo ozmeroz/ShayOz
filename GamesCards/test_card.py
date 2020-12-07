a15287c3a4c7dfd49ddf768da3080cc5b4cae782
shay <shay2908#gmail.com>
2020-12-07 14:11:51 +0200

    בדיקה האם הקלף אינו גדול מ13 או קטן מace

--- GamesCards/test_card.py
+++ GamesCards/test_card.py
@@ -12,9 +12,14 @@ class TestCard(TestCase):
         self.c5=Card(5,2)
         self.c6=Card(1,4)
         self.c7=Card(15,2)
+        self.c8=Card(10,6)
 
 
     def test_compare(self):
+        self.assertIn("Invalid",self.c7.compare(self.c6))
+        self.assertIn("Invalid", self.c6.compare(self.c7))
+        self.assertIn("Invalid", self.c8.compare(self.c6))
+        self.assertIn("Invalid", self.c6.compare(self.c8))
         self.assertEqual(self.c1.compare(self.c2),self.c2) # בדיקה האם הקלף הראשון שווה לאס והקלף השני לא, הפונקציה תחזיר את האס
         self.assertEqual(self.c2.compare(self.c1), self.c2) # בדיקה האם הקלף השני שווה לאס והראשון לא , הפונקציה תחזיר את האס
         self.assertEqual(self.c4.compare(self.c5), self.c4) # בדיקה כאשר הקלף הראשון שווה לקלף השני בערכו , הפונקציה תחזיר את הקלף שה-suit שלו גדול יותר
@@ -22,9 +27,12 @@ class TestCard(TestCase):
         self.assertEqual(self.c5.compare(self.c1),self.c1) # בודקת האם הפונקציה מחזירה את הקלף עם הערך הגדול יותר
         self.assertEqual(self.c1.compare(self.c5), self.c1) # בודקת האם הפונקציה מחזירה את הקלף עם הערך הגדול יותר ללא תלות במי הקלף הראשון ומי השני
         self.assertEqual(self.c2.compare(self.c6), self.c6) # בודקת שכאשר שני הקלפים שווים לAce הפונקציה תחזיר את הAce עם הsuit הגדול יותר
-        # with self.assertRaises(AttributeError):
-        #     self.c1.compare(self.c7)
-        # self.assertEqual(self.c5.compare(self.c7), TypeError)
+
+
+
+
+
+
 
 
 
